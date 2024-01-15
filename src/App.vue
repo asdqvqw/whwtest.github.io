5f12ff702f773dd3f6d82df9359f1e0c82e6f37b
whwwhw <ulwanshiwu@163.com>
2024-01-15 16:17:22 +0800

    无

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <h1 class="title">Welcome to Bulma Example</h1>
+      <p class="subtitle">A simple example using Bulma CSS framework</p>
+      
+      <div class="columns">
+        <div class="column">
+          <div class="box">
+            <p class="title is-4">Column 1</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+        
+        <div class="column">
+          <div class="box">
+            <p class="title is-4">Column 2</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
