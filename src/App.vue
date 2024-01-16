6f5883c688a322866a416962ad1827c3936d5153
whwwhw <ulwanshiwu@163.com>
2024-01-16 14:11:35 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="love-effect">
+    <transition name="fade">
+      <h1 v-if="showMessage" class="message">赵佳宁是大傻瓜！</h1>
+    </transition>
+    <button class="btn"  v-if="!showMessage" @click="showMessage = !showMessage">点这查看王浩文的心事</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      showMessage: false
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.love-effect {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(45deg, #ff4d4d, #ff99cc);
+}
+
+.message {
+  font-size: 3rem;
+  color: white;
+  animation: pulse 1s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.btn {
+  padding: 10px 20px;
+  font-size: 1.5rem;
+  background-color: #ff99cc;
+  border: none;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.btn:hover {
+  background-color: #ff4d4d;
 }
 </style>
