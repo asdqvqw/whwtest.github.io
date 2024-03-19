d37b3f617042106aca58bb53fc7f61768116e495
root <root@LAPTOP-OAP9BCO1>
2024-03-19 18:30:44 +0800

    111

---
+++ docs/errorbak.js
@@ -0,0 +1,15 @@
+// import {ref } from 'vue'
+
+// export const jsondata = ref({
+//     data: [
+
+//     ],
+//   });
+
+
+export default {
+  data: [
+    { name: "控制台IP", key: "IP" },
+    { name: "备用IP", key: "BACKUPIP" },
+  ]
+};
